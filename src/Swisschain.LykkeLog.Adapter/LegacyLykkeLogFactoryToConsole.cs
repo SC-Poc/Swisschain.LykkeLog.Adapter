7e0a9b07af17d9c1eeae95c7fa6f6e0d45a65ecd
Alexey Novichikhin <novichikhin.au@gmail.com>
2020-03-09 17:20:39 +0200

    add log to console

---
+++ src/Swisschain.LykkeLog.Adapter/LegacyLykkeLogFactoryToConsole.cs
@@ -0,0 +1,33 @@
+﻿namespace Swisschain.LykkeLog.Adapter
+{
+    public class LegacyLykkeLogFactoryToConsole : Lykke.Common.Log.ILogFactory
+    {
+        public static Lykke.Common.Log.ILogFactory Instance
+        {
+            get
+            {
+                return new LegacyLykkeLogFactoryToConsole();
+            }
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public Common.Log.ILog CreateLog<TComponent>(TComponent component, string componentNameSuffix)
+        {
+            return new LegacyLykkeLogToConsole();
+        }
+
+        public Common.Log.ILog CreateLog<TComponent>(TComponent component)
+        {
+            return new LegacyLykkeLogToConsole();
+        }
+
+        public void AddProvider(Microsoft.Extensions.Logging.ILoggerProvider provider)
+        {
+
+        }
+    }
+}
---
+++ src/Swisschain.LykkeLog.Adapter/LegacyLykkeLogToConsole.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lykke.Common.Log;
+
+namespace Swisschain.LykkeLog.Adapter
+{
+    public class LegacyLykkeLogToConsole : Common.Log.ILog
+    {
+        public void Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, Microsoft.Extensions.Logging.EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) where TState : LogEntryParameters
+        {
+            Console.WriteLine($"{logLevel}\t{formatter.Invoke(state, exception)}");
+        }
+
+        public bool IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public IDisposable BeginScope(string scopeMessage)
+        {
+            return new EmptyDisposable();
+        }
+
+        public Task WriteInfoAsync(string component, string process, string context, string info, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"INFO\t{component};{process};{info};{context}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteMonitorAsync(string component, string process, string context, string info, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"MONITOR\t{component};{process};{info};{context}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteWarningAsync(string component, string process, string context, string info, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"WARNING\t{component};{process};{info};{context}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteWarningAsync(string component, string process, string context, string info, Exception ex,
+            DateTime? dateTime = null)
+        {
+            Console.WriteLine($"WARNING\t{component};{process};{info};{context}\n\r{ex}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteErrorAsync(string component, string process, string context, Exception exception, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"ERROR\t{component};{process};{exception.Message};{context}\n\r{exception}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteFatalErrorAsync(string component, string process, string context, Exception exception,
+            DateTime? dateTime = null)
+        {
+            Console.WriteLine($"WARN\t{component};{process};{exception.Message};{context}\n\r{exception}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteInfoAsync(string process, string context, string info, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"INFO\t{process};{info};{context}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteMonitorAsync(string process, string context, string info, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"MONITOR\t{process};{info};{context}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteWarningAsync(string process, string context, string info, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"WARNING\t{process};{info};{context}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteWarningAsync(string process, string context, string info, Exception ex, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"WARNING\t{process};{info};{context}\n\r{ex}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"ERROR\t{process};{exception.Message};{context}\n\r{exception}");
+            return Task.CompletedTask;
+        }
+
+        public Task WriteFatalErrorAsync(string process, string context, Exception exception, DateTime? dateTime = null)
+        {
+            Console.WriteLine($"FATAL-ERROR\t{process};{exception.Message};{context}\n\r{exception}");
+            return Task.CompletedTask;
+        }
+
+        public class EmptyDisposable : IDisposable
+        {
+            public void Dispose()
+            {
+
+            }
+        }
+    }
+}
